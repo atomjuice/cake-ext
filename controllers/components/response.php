e315946836bad6c5bc2b85ac87d788d44f43324a
Kevin Andrews <kevin.andrews@atomjuice.com>
2017-03-13 14:42:24 +0000

    Added constants for http codes.

--- controllers/components/response.php
+++ controllers/components/response.php
@@ -5,35 +5,58 @@
  * @author kandrews
  */
 class ResponseComponent extends Component {
-    
+
+    /** HTTP Response Code Constants */
+    const HTTP_OK = 200;
+    const HTTP_CREATED = 201;
+    const HTTP_ACCEPTED = 202;
+    const HTTP_NO_CONTENT = 204;
+
+    const HTTP_MOVED_PERMANENTLY = 301;
+    const HTTP_FOUND = 302;
+    const HTTP_NOT_MODIFIED = 304;
+    const HTTP_TEMPORARY_REDIRECT = 307;
+
+    const HTTP_BAD_REQUEST = 400;
+    const HTTP_UNAUTHORIZED = 401;
+    const HTTP_FORBIDDEN = 403;
+    const HTTP_NOT_FOUND = 404;
+    const HTTP_METHOD_NOT_ALLOWED = 405;
+    const HTTP_NOT_ACCEPTABLE = 406;
+    const HTTP_TIME_OUT = 408;
+    const HTTP_TOO_MANY_REQUESTS = 429;
+
+    const HTTP_INTERNAL_SERVER_ERROR = 500;
+    const HTTP_NOT_IMPLEMENTED = 501;
+
     public $components = array('Ext.Request');
-    
+
     /**
-     * @var RequestComponent 
+     * @var RequestComponent
      */
     public $Request;
-    
+
     /**
-     * @var XmlHelper 
+     * @var XmlHelper
      */
     public $XmlHelper;
-    
+
     private $statusCode = 200;
     private $contentName = 'html';
     private $contentType = 'text/html';
     private $charset = 'UTF-8';
     private $headers = array();
-    
+
     /**
      * {@inheritdoc}
      */
     public function initialize(&$controller) {
-        
+
         if($this->Request->requestedWith() === 'xml') {
             App::import('Helper', 'Xml');
             $this->XmlHelper = new XmlHelper();
         }
-        
+
         if($this->Request->requestedWith() === 'xml' || $this->Request->requestedWith() === 'json') {
             $this->contentName = $this->RequestHandler->requestedWith();
             $this->contentType = 'application/' . $this->Request->requestedWith();
@@ -41,27 +64,27 @@ class ResponseComponent extends Component {
 
         parent::initialize($controller);
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function beforeRedirect(&$controller, $url, $status = null) {
         parent::beforeRedirect($controller, $url, $status);
     }
-    
+
     /**
      * Http Response Code
      * @link https://en.wikipedia.org/wiki/List_of_HTTP_status_codes List of valid response codes
      * @param integer $code
      */
-    public function setStatusCode($code) 
-    {    
+    public function setStatusCode($code)
+    {
         if($this->isValidStatusCode($code)) {
             $this->statusCode = $code;
         }
         return $this;
     }
-    
+
     /**
      * Set response content type headers
      * @param string $name json | xml
@@ -69,16 +92,16 @@ class ResponseComponent extends Component {
      * @return boolean
      */
     public function setContentType($name, $type) {
-        
+
         if($this->isValidContentType($name, $type)) {
             $this->contentName = $name;
             $this->contentType = $type;
         }
         return $this;
     }
-    
+
     /**
-     * 
+     *
      * @param string $name
      * @param string $value
      * @param boolean $replace
@@ -89,7 +112,7 @@ class ResponseComponent extends Component {
         }
         return $this;
     }
-    
+
     /**
      * Called by the controller automatically and uses private method send() to setup response
      * @param Controller $controller
@@ -100,12 +123,12 @@ class ResponseComponent extends Component {
         if(isset($controller->viewVars['response'])) {
             $controller->viewVars['response'] = $render($controller->viewVars['response']);
         }
-        
+
         $this->send();
-        
+
         parent::beforeRender($controller);
     }
-    
+
     /**
      * Setup response before rendering the view
      * Hooked into the controller beforeRender(); you don't have to run this
@@ -114,18 +137,18 @@ class ResponseComponent extends Component {
     {
         http_response_code($this->statusCode);
         header($this->getContentType(), true);
-        
+
         foreach ($this->getHeaders() as $header) {
             header($header);
         }
     }
-    
+
     private function getRenderer()
     {
         $renderer = function($var) {
             return $var;
         };
-        
+
         if($this->contentName === 'xml') {
             $xmlHelper = $this->XmlHelper;
             $renderer = function($var) use ($xmlHelper) {
@@ -136,10 +159,10 @@ class ResponseComponent extends Component {
                 return json_encode($var);
             };
         }
-        
+
         return $renderer;
     }
-    
+
     /**
      * Returns an array of headers converted to strings
      * @return array
@@ -152,22 +175,22 @@ class ResponseComponent extends Component {
         }
         return $headers;
     }
-    
+
     private function getContentType() {
         header('Content-type: ' . $this->contentType . '; charset=' . $this->charset, true);
     }
-    
+
     /**
-     * 
+     *
      * @param integer $code
      * @return boolean
      */
     private function isValidStatusCode($code) {
-        
+
         if(!is_numeric($code) || strlen($code) !== 3) {
             return false;
         }
-        
+
         $validCodes = array(
             200 => 'OK',
             201 => 'Created',
@@ -179,31 +202,31 @@ class ResponseComponent extends Component {
             405 => 'Method Not Allowed',
             406 => 'Not Accepted',
         );
-        
+
         return array_key_exists($code, $validCodes);
     }
-    
+
     /**
-     * 
+     *
      * @param string $name json | xml | other
      * @param string $type application/json | application/xml | other
      * @return boolean
      */
     private function isValidContentType($name, $type) {
-        
+
         $return = false;
-        
+
         $validTypes = array(
             'json' => 'application/json',
             'xml' => 'application/xml',
             'html' => 'text/html',
         );
-        
+
         if(array_key_exists($name, $validTypes) && $validTypes[$name] === $type) {
             $return = true;
         }
-        
+
         return $return;
     }
-    
+
 }
