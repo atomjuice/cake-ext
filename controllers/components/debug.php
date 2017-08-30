e09179ba64f387b42ab40b56bab10bab9f55dd64
Kevin Andrews <kevin.andrews@atomjuice.com>
2017-08-30 15:44:58 +0100

    Added basic timeline measuring and formatted code.

--- controllers/components/debug.php
+++ controllers/components/debug.php
@@ -4,58 +4,79 @@ use \DebugBar\StandardDebugBar;
 
 /**
  * Access class for the DebugBar, makes sure there is only one instance of the DebugBar
- * Use of this class requires phpdebugbar to be loaded via composer and composer autoloading to be 
- * initialised as part of the cake front controller or bootstrap.php 
- * 
+ * Use of this class requires phpdebugbar to be loaded via composer and composer autoloading to be
+ * initialised as part of the cake front controller or bootstrap.php
+ *
  * Also assets need to be copied from vendor/ to public_html/vendor to make js/css/imgs available to browsers.
- * 
- * @link http://phpdebugbar.com/docs/ 
+ *
+ * @link http://phpdebugbar.com/docs/
  */
-class DebugComponent extends Component {
-    
-    public function __construct() {
-        parent::__construct();
-    }
-    
+class DebugComponent extends Component
+{
     /**
-     * 
      * @return StandardDebugBar
      */
     public function getInstance() {
         return ClassRegistry::getInstance()->getObject('debugBar');
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function initialize(&$controller, $settings=array()) {
         $debugBar = new StandardDebugBar();
         ClassRegistry::getInstance()->addObject('debugBar', $debugBar);
+
+        $this->getInstance()['time']->startMeasure(get_class($controller) . '_controller', 'Starting ' . get_class($controller) . '.');
+        $this->getInstance()['time']->startMeasure(get_class($controller) . '_controller_beforefilter', 'Starting ' . get_class($controller) . ' beforeFilter.');
+
         App::import('Lib', 'Ext.MysqliCollector');
-        
         try {
             $this->getInstance()->getCollector('mysqli');
         } catch (Exception $ex) {
-            $this->getInstance()->addCollector(new Mysqli_Collector());     
+            $this->getInstance()->addCollector(new Mysqli_Collector());
         }
+
+        parent::initialize($controller);
     }
-    
+
+    /**
+     * {@inheritdoc}
+     */
+    public function startup(&$controller)
+    {
+        $this->getInstance()['time']->stopMeasure(get_class($controller) . '_controller_beforefilter');
+        $this->getInstance()['time']->startMeasure(get_class($controller) . '_controller_action', 'Starting ' . get_class($controller) . '->' . $controller->action . ' action.');
+        parent::startup($controller);
+    }
+
     /**
      * {@inheritdoc}
      */
     public function beforeRedirect(&$controller, $url, $status = null, $exit = true) {
         parent::beforeRedirect($controller, $url, $status, $exit);
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function beforeRender(&$controller)
     {
+        $this->getInstance()['time']->stopMeasure(get_class($controller) . '_controller_action');
+        $this->getInstance()['time']->stopMeasure(get_class($controller) . '_controller');
+
         $debugbarRenderer = $this->getInstance()->getJavascriptRenderer();
         $debugbarRenderer->setBaseUrl('/ext/');
         $controller->set('debugbarRenderer', $debugbarRenderer);
         $controller->set('debugShow', (Configure::read('PRODUCTION')) ? ((Configure::read('debug') < 2) ? false : true) : true);
         parent::beforeRender($controller);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function shutdown(&$controller)
+    {
+        parent::shutdown($controller);
     }
 }
